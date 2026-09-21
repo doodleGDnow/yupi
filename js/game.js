35a4a2fbf3cab6b703feff619c38bda20216c30a
doodleGDnow <doodlegdxd@gmail.com>
2026-09-21 16:27:06 -0500

    Add event listeners for play and zuno buttons

--- js/game.js
+++ js/game.js
@@ -1,1 +1,10 @@
+const playButton = document.getElementById("playButton");
+const zunoButton = document.getElementById("zunoButton");
 
+playButton.addEventListener("click", () => {
+    console.log("play");
+});
+
+zunoButton.addEventListener("click", () => {
+    console.log("zuno");
+});
